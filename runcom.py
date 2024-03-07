171070e0cfdec381e299b85524ad7c8d81ae0a17
namho-96 <65693870+namho-96@users.noreply.github.com>
2024-03-07 09:35:21 +0900

    예제 run 파일 작성

--- runcom.py
+++ runcom.py
@@ -1,3 +1,5 @@
 import os
 
-os.system('python main.py --config config_abaw_au --data_type spatial')
+#os.system('python main.py --device 0 --data_name va --num_features 1024 --num_classes 1 --train_name test_va --lr 0.00001 --batch_size 512 --num_workers 4 --model_arch self mix mix forward neck va')
+#os.system('python main.py --device 0 --data_name au --num_features 1024 --num_classes 8 --train_name test_au --lr 0.00001 --batch_size 512 --num_workers 4 --model_arch self mix mix forward neck au')
+#os.system('python main.py --device 0 --data_name expr --num_features 1024 --num_classes 8 --train_name test_expr --lr 0.00001 --batch_size 512 --num_workers 4 --model_arch self mix mix forward neck expr')
